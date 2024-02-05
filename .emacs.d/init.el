a00871b96198b0b2aeb0b515a0f92adc051cef9e
kimikoluv1996 <kimikoluv1996@proton.me>
2024-02-05 10:44:22 -0500

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(emms smart-mode-line powerline gcmh general undo-fu markdown-mode evil-collection)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
