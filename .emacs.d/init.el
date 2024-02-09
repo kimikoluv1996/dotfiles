67a7b0a6dfb65399540282adc47edd26f5a15228
kimikoluv1996 <kimikoluv1996@proton.me>
2024-02-08 19:19:22 -0500

    move everything to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,58 @@
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+  (require 'package)
+  (add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(emms smart-mode-line powerline gcmh general undo-fu markdown-mode evil-collection)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; install use-package
+
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+;; reduce garbage collection to make startup faster
+  (use-package gcmh
+    :ensure t
+    :config
+    (gcmh-mode 1))
+
+  (setq gc-cons-threshold 402653184
+	 gc-cons-percentage 0.6)
+
+  (add-hook 'emacs-startup-hook
+	   (lambda ()
+		(message "*** Emacs loaded in %s with %d garbage collections."
+		   (format "%.2f seconds"
+			     (float-time
+			       (time-subtract after-init-time before-init-time)))
+		    gcs-done)))
+
+;; silence compiler warnings
+(setq comp-async-report-warnings-errors nil)
+
+;; change font
+  (add-to-list 'default-frame-alist '(font . "Jetbrains Mono"))
+  (set-face-attribute 'default t :font "Jetbrains Mono")
+
+;; remove stuff for babbies like scroll bars
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+
+  (global-display-line-numbers-mode 1)
+  (global-visual-line-mode t)
+
+;; install doom-themes
+   (use-package doom-themes :ensure t)
+   (setq doom-themes-enable-bold t)
+   (setq doom-themes-enable-italic t)
+   (load-theme 'doom-one t)
+
+;; use smart modeline
+  (use-package smart-mode-line :ensure t)
+  (sml/setup)
+
+;; emms
+  (use-package emms :ensure t)
+  (require 'emms-setup)
+  (emms-all)
+  (emms-default-players)
